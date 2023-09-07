0e8952d0b46a0a91cc53e3f0667f019975b73c62
Laurentiu Ungur <laurentyu1995@gmail.com>
2023-09-07 09:54:50 +0300

    Replace deprecated CC_MD5

--- Sources/Extension/String+Ext.swift
+++ Sources/Extension/String+Ext.swift
@@ -7,21 +7,10 @@
 //
 
 import Foundation
-import CommonCrypto
+import CryptoKit
 
 extension String {
-    
     var videoCacheMD5: String {
-        let str = cString(using: String.Encoding.utf8) ?? []
-        let strLen = CUnsignedInt(lengthOfBytes(using: String.Encoding.utf8))
-        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
-        CC_MD5(str, strLen, result)
-        let hash = NSMutableString()
-        for i in (0..<digestLen) {
-            hash.appendFormat("%02x", result[i])
-        }
-        defer { result.deallocate() }
-        return String(format: hash as String)
+        Insecure.MD5.hash(data: Data(utf8)).map { String(format: "%02hhx", $0) }.joined()
     }
 }
